846ad9dc9abedaefc0f526e95977e8513cae295a
kedar1704 <119061633+kedar1704@users.noreply.github.com>
2022-11-29 12:02:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "dev_machine" {
   ami = "ami-094125af156557ca2"
   instance_type = "t2.micro"
-  vpc_id = "vpc-092ff930d4461e6f3"
   subnet_id = "subnet-069e212944d2fd228"
   associate_public_ip_address = true
   availability_zone = "us-west-2a"
