99ab0159a9e49a2dd09e2477760eb1a9d3df91be
kedar1704 <119061633+kedar1704@users.noreply.github.com>
2022-11-29 11:48:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "dev_machine" {
   ami = "ami-094125af156557ca2"
   instance_type = "t2.micro"
-  subnet_id  = aws_subnet.dev-public-1.id
+  subnet_id  = aws_subnet.project-subnet-public1-us-west-2a.id
   availability_zone = "us-west-2a"
   key_name = "EC2_Oregon"
 
